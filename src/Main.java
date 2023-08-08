a3d938b0623feafa35d30ff4b809240c5409adcf
Homework3 <tradervdovin@gmail.com>
2023-08-08 09:20:29 +0500

    2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         // Проверка работы программы
-        System.out.println(PalindromeChecker.isPalindrome("А роза упала на лапу Азора"));  // true
-        System.out.println(PalindromeChecker.isPalindrome("Hello World"));  // false
-        System.out.println(PalindromeChecker.isPalindrome("12321"));  // true
+
+        System.out.println(PalindromeChecker.isPalindrome("Аргентина манит негра"));  // true
+        System.out.println(PalindromeChecker.isPalindrome("Hello, world!")); // false
     }
 }
